28af74d313fe02b18b6114000ae46a17cad646df
Ljove87 <veljko33614@its.edu.rs>
2019-09-19 12:55:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+const App = () => {
+  
+ 
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = () => {
+  
+    return (
+        <div>
+            <Part name="Fundamentals of React" amount="10" />
+            <Part name="Using props to pass data" amount="7" />
+            <Part name="State of a component" amount="14" />
+        </div>
+    )
+}
+
+export default Content
